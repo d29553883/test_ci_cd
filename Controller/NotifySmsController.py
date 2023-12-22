918dbccf82909bbe0897ec8f0021a8f42c8f70f4
David Lin <d2955388365@gmail.com>
2023-12-22 15:59:12 +0800

    test ci cd5

--- Controller/NotifySmsController.py
+++
@@ -1,6 +0,0 @@
-from Model.notice import *
-import json
-import collections
-import requests, pickle
-from multiprocessing.dummy import Pool as ThreadPool
-
--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import uvicorn
-import atexit
 from fastapi import FastAPI, Request, Header, UploadFile, File
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from Controller.FirebaseController import *
-from middleware import *
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 import os, sys
 
 
@@ -29,58 +24,6 @@ app = FastAPI(
 @app.get("/")
 async def index():
     return {"message": "Access Denied"}
-
-
-@app.post(prefix + "/v1/notifiaction/save")
-def create_db(data: notify, request: Request):
-    return FirebaseController.saveList(data)
-
-
-# 測試自動發送email
-@app.patch(prefix + "/v1/notifiaction/send_email")
-def AutoSendEmail():
-    return FirebaseController.auto_send_email()
-
-
-# 測試自動發送sms
-@app.get(prefix + "/v1/notifiaction/send_sms")
-def AutoSendSms():
-    return FirebaseController.auto_send_sms()
-
-
-# 測試自動發送fcm
-@app.get(prefix + "/v1/notifiaction/send_fcm")
-def AutoSendFcm():
-    return FirebaseController.auto_send_fcm()
-
-
-# 測試立即發送sms
-@app.post(prefix + "/v1/notifiaction/right_now_sms")
-def RightNowSms(data: notify):
-    return FirebaseController.right_now_sms(data)
-
-
-# 測試立即發送email
-@app.post(prefix + "/v1/notifiaction/right_now_email")
-def RightNowEmail(data: notify):
-    return FirebaseController.right_now_email(data)
-
-
-# 測試存brand 資訊
-@app.post(prefix + "/v1/notify_brand/save")
-def create_db(data: brand):
-    return FirebaseController.save_brand(data)
-
-
-@app.get(prefix + "/v1/get_ip")
-def get_ip():
-    ip = requests.get("https://api.ipify.org").text
-    return ip
-
-
-@app.post(prefix + "/v1/send_email_with_products")
-def send_email_with_products(data: dict):
-    return FirebaseController.send_email_with_products(data)
 
 
 
